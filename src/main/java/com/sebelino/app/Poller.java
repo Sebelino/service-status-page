8569f81f1f2f0862c14d99015ec5ae6d68772f8e
Sebastian Olsson <sebelino7@gmail.com>
2022-04-22 19:10:44 +0200

    refactor: Simplify lambda

--- src/main/java/com/sebelino/app/Poller.java
+++ src/main/java/com/sebelino/app/Poller.java
@@ -6,11 +6,9 @@ public class Poller extends AbstractVerticle {
     @Override
     public void start() {
         vertx.createHttpServer().requestHandler(
-                req -> {
-                    req.response()
-                            .putHeader("content-type", "text/plain")
-                            .end("Hello from vert.x!");
-                }
+                req -> req.response()
+                        .putHeader("content-type", "text/plain")
+                        .end("Hello from vert.x!")
         ).listen(8080);
     }
 }
