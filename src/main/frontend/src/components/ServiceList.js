565279ba28fcaddd67583746ad650a1ef1ef6b49
Sebastian Olsson <sebelino7@gmail.com>
2022-04-23 20:39:10 +0200

    chore: Remove unneeded UI elements

--- src/main/frontend/src/components/ServiceList.js
+++ src/main/frontend/src/components/ServiceList.js
@@ -16,10 +16,6 @@ export default class ServiceList extends Component {
     }
 
     render() {
-        const {
-            items, updateTodosToShow, clearList, handleDelete, handleEdit, handleDoneTask, handleDeleteDoneTasks
-        } = this.props
-
         return (<Fragment>
             <h3 className="text-center">
                 ServiceList
@@ -32,20 +28,6 @@ export default class ServiceList extends Component {
                         id={item.id}
                         name={item.name}
                         url={item.url}
-                    />)
-                })}
-            </ul>}
-
-            {items.length === 0 ? '' : <ul className="list-group my-5">
-                {items.map(item => {
-                    return (<ServiceItem
-                        key={item.id}
-                        id={item.id}
-                        name={item.title}
-                        completed={item.completed}
-                        handleDelete={() => handleDelete(item.id)}
-                        handleEdit={() => handleEdit(item.id)}
-                        handleDoneTask={handleDoneTask}
                     />)
                 })}
             </ul>}
