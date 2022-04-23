271ffcedb25ede2057b21ed9c15185ec56ebb072
Sebastian Olsson <sebelino7@gmail.com>
2022-04-23 16:01:26 +0200

    test: Test POST + GET

--- src/test/java/com/sebelino/app/TestMainVerticle.java
+++ src/test/java/com/sebelino/app/TestMainVerticle.java
@@ -7,6 +7,7 @@ import io.vertx.ext.web.codec.BodyCodec;
 import io.vertx.junit5.Timeout;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -43,6 +44,34 @@ public class TestMainVerticle {
             webClient.post(MainVerticle.PORT, "localhost", "/status").as(BodyCodec.string()).sendBuffer(payload.toBuffer(), testContext.succeeding(resp -> {
                 testContext.verify(() -> {
                     assertThat(resp.statusCode()).isEqualTo(201);
+                    testContext.completeNow();
+                });
+            }));
+        }));
+    }
+
+    @Test
+    @DisplayName("POST /status successfully, then GET it")
+    @Timeout(value = 9, timeUnit = TimeUnit.SECONDS)
+    void postAndGet(Vertx vertx, VertxTestContext testContext) {
+        String serviceName = RandomStringUtils.randomAlphabetic(10);
+        String serviceUrl = String.format("https://%s.com", serviceName);
+        JsonObject payload = new JsonObject().put("name", serviceName).put("url", serviceUrl);
+        WebClient webClient = WebClient.create(vertx);
+        vertx.deployVerticle(new MainVerticle(), testContext.succeeding(id -> {
+            webClient.post(MainVerticle.PORT, "localhost", "/status").as(BodyCodec.string()).sendBuffer(payload.toBuffer(), testContext.succeeding(resp -> {
+                testContext.verify(() -> {
+                    assertThat(resp.statusCode()).isEqualTo(201);
+                    testContext.completeNow();
+                });
+            }));
+            webClient.get(MainVerticle.PORT, "localhost", "/status").as(BodyCodec.string()).sendBuffer(payload.toBuffer(), testContext.succeeding(resp -> {
+                testContext.verify(() -> {
+                    assertThat(resp.statusCode()).isEqualTo(200);
+                    JsonObject body = new JsonObject(resp.body());
+                    Status status = body.mapTo(Status.class);
+                    Service retrievedService = status.services.stream().filter(s -> s.name.equals(serviceName)).findAny().orElseThrow();
+                    assertThat(retrievedService.url).isEqualTo(serviceUrl);
                     testContext.completeNow();
                 });
             }));
