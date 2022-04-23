ca7bb5a5f99cc5a2903bcd571bc8e51c017d0a50
Sebastian Olsson <sebelino7@gmail.com>
2022-04-23 16:42:43 +0200

    feat: Output createdAt as ISO 8601 string

--- src/main/java/com/sebelino/app/Service.java
+++ src/main/java/com/sebelino/app/Service.java
@@ -1,10 +1,13 @@
 package com.sebelino.app;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 
 public class Service {
     public String name;
     public String url;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "uuuu-MM-dd'T'HH:mm:ss.SSS")
     public LocalDateTime createdAt;
 }
