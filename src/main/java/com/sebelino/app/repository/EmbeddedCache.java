dea0d438c2dc73e499dff123f400558ca23b4992
Sebastian Olsson <sebelino7@gmail.com>
2022-04-24 11:59:57 +0200

    feat: Add methods to embedded cache

--- src/main/java/com/sebelino/app/repository/EmbeddedCache.java
+++ src/main/java/com/sebelino/app/repository/EmbeddedCache.java
@@ -1,7 +1,16 @@
 package com.sebelino.app.repository;
 
+import java.util.HashMap;
 import java.util.Map;
 
 class EmbeddedCache {
-    private Map<String, Boolean> urlToStatus;
+    private final Map<String, String> urlToStatus = new HashMap<>();
+
+    public void put(String url, String status) {
+        urlToStatus.put(url, status);
+    }
+
+    public String get(String url) {
+        return urlToStatus.get(url);
+    }
 }
