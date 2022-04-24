d5415b2f1c8081fb8faff074bf1386db6c5a4d4f
Sebastian Olsson <sebelino7@gmail.com>
2022-04-24 12:39:02 +0200

    feat: Poll Google

--- src/main/java/com/sebelino/app/PollerVerticle.java
+++ src/main/java/com/sebelino/app/PollerVerticle.java
@@ -1,12 +1,26 @@
 package com.sebelino.app;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.web.codec.BodyCodec;
 
 public class PollerVerticle extends AbstractVerticle {
+
+    private WebClient client;
+
     @Override
     public void start() {
-        vertx.setPeriodic(1000, r -> {
-            System.out.println("HOY");
+        client = WebClient.create(vertx);
+
+        vertx.setPeriodic(3000, r -> {
+            client.get(80, "google.com", "/")
+                    .as(BodyCodec.string()).send(response -> {
+                        if (response.succeeded()) {
+                            System.out.println("Website is up");
+                        } else {
+                            System.out.println("Connection refused");
+                        }
+                    });
         });
     }
 }
