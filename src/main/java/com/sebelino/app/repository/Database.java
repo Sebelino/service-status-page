0bfcdee4688a64f7ddf850088467e8e09ae8aaba
Sebastian Olsson <sebelino7@gmail.com>
2022-04-24 11:18:43 +0200

    refactor: Make package-private

--- src/main/java/com/sebelino/app/repository/Database.java
+++ src/main/java/com/sebelino/app/repository/Database.java
@@ -15,7 +15,7 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
-public class Database {
+class Database {
 
     private final MySQLPool pool;
 
